528e7be7cb3679f9f92341256931279386bd5b4c
issamelferkh <issam.elferkh@gmail.com>
2020-06-12 20:55:38 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person';
+
+// // default function
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// // with function
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hi I'm a react App</h1>
+//       <p>This is working</p>
+//     </div>
+//   );
+// }
+
+// with class
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>Hi I'm a react App</h1>
+        <p>This is working</p>
+        <Person />
+      </div>
+    );
+  }
+  // return React.createElement('div', {className:"App"}, 
+  // React.createElement('h1', null, 'Hi I\'m a react App'));
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// const person = () => {
+//     return (
+//         <h1>I'm a Person compenent from Person/Person.js</h1>
+//     );
+// }
+
+// const person = function() {
+//     return (
+//         <h1>I'm a Person compenent from Person/Person.js</h1>
+//     )
+// }
+
+
+
+export default person;

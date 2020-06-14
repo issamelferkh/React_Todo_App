cadbd9c7b00dec01e87b93081a68faec0a23ba73
issamelferkh <issam.elferkh@gmail.com>
2020-06-14 01:44:03 +0000

    add react app todo list

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# React Todo App
+
+https://github.com/kabirbaidhya/react-todo-app
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,53 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person';
 
-// // default function
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// // with function
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hi I'm a react App</h1>
-//       <p>This is working</p>
-//     </div>
-//   );
-// }
-
-// with class
 class App extends Component {
-  render () {
-    return (
-      <div className="App">
-        <h1>Hi I'm a react App</h1>
-        <p>This is working</p>
-        <Person />
+  constructor(props) {
+    super(props);
+
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+
+  updateInput(key, value) {
+    //update react state
+    this.setState({
+      [key]:value
+    });
+  }
+
+  addItem() {
+    // create Item with unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+    // copy current list of items
+    const list = [...this.state.list];
+    // add new item to list
+    list.push(newItem);
+    // update state with new list and reset newItem input
+    this.setState({
+      list,
+      newItem:""
+    });
+  }
+
+  deleteItem(id) {
+    // copy current list of items
+    const list = [...this.state.list]; 
+    // filter out item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({list: updatedList});
+  }
+  
+  render() {
+    return(
+      <div>
+        <h1 className="app-title">MY LIST</h1>
+        <div className="container">
+          <div className="sub_container">
+            Add an Item... <br />
+            <input type="text" placeholder="Type item here" value={this.state.newItem} onChange={e => this.updateInput("newItem", e.target.value)}></input>
+            <button className="add-btn" onClick={() => this.addItem()} >Add</button>
+            <br /> <br />
+            <ul>
+              {this.state.list.map(item => {
+                return(
+                  <a onClick={() => this.deleteItem(item.id)}>
+                    <li key={item.id}>{item.value}</li>
+                  </a>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
-  // return React.createElement('div', {className:"App"}, 
-  // React.createElement('h1', null, 'Hi I\'m a react App'));
 }
 
 export default App;
